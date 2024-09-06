1c0b15494d0c14695fc7778eae47f45f70dcf2b6
ErickJMenezes <erickmenezes.dev@gmail.com>
2024-09-05 22:06:46 -0300

    Add readonly attribute to UnknownDirectiveException
    This commit introduces a readonly attribute for the directive name in the UnknownDirectiveException class. The directive name is now set as a readonly property, ensuring it cannot be modified after initialization.

--- src/Exceptions/UnknownDirectiveException.php
+++ src/Exceptions/UnknownDirectiveException.php
@@ -9,8 +9,11 @@ use Throwable;
 
 class UnknownDirectiveException extends InvalidArgumentException
 {
+    public readonly string $directiveName;
+
     public function __construct(string $directiveName, Throwable $previous = null)
     {
+        $this->directiveName = $directiveName;
         parent::__construct(
             "The directive [$directiveName] does not exist. And is not supported by browsers.",
             1000,
